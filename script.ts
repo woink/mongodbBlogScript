0032090bd84a5c26f9b2582980f7e27b994016d7
woink <wardprice@gmail.com>
2021-01-31 22:12:18 -0500

    refactor: :fire: removed unused import (Bson) from deno_mongo

--- script.ts
+++ script.ts
@@ -1,5 +1,5 @@
 import { faker } from 'https://deno.land/x/deno_faker@v1.0.3/mod.ts';
-import { MongoClient, Bson } from 'https://deno.land/x/mongo@v0.21.0/mod.ts';
+import { MongoClient } from 'https://deno.land/x/mongo@v0.21.0/mod.ts';
 
 const client = new MongoClient();
 await client.connect("mongodb://localhost:27017")
