fe3270e64987e782c41257309d3143f9e2587efc
woink <wardprice@gmail.com>
2021-01-31 22:10:38 -0500

    fix: :ambulance: fix async issue, now working

--- script.ts
+++ script.ts
@@ -1,18 +1,25 @@
-import { faker } from "https://deno.land/x/deno_faker@v1.0.3/mod.ts"
-import { MongoClient, Bson } from "https://deno.land/x/mongo@v0.21.0/mod.ts";
+import { faker } from 'https://deno.land/x/deno_faker@v1.0.3/mod.ts';
+import { MongoClient, Bson } from 'https://deno.land/x/mongo@v0.21.0/mod.ts';
 
-const client = new MongoClient()
-await client.connect("mongodb://127.0.0.1:27017")
+const client = new MongoClient();
+await client.connect("mongodb://localhost:27017")
 
-const db = client.database("lookup")
-const peopleCollection = db.collection("people")
+interface PeopleSchema {
+  _id: { $oid: string };
+  name: string;
+  age: number;
+}
 
-const firstName = faker.name.firstName()
-const age = faker.random.number(80)
+const db = client.database('lookup');
+const people = db.collection<PeopleSchema>('people');
 
-for (let i = 0; i < 50; i++) {
-  peopleCollection.insertOne({
+const firstName = faker.name.firstName();
+const age = faker.random.number(80);
+
+
+for (let i = 0; i < 1000000; i++) {
+	await people.insertOne({
     name: firstName,
-    age: age
-  })
+    age: age,
+  });
 }
